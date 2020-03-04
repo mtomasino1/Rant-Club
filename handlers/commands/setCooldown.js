8e4d59f02d53946bbaae270a5a7e9e94df8e78db
James McKay <james.mckay215@yahoo.com>
2020-03-04 09:57:14 -0800

    Added some simple error handling for setCooldown

---
+++ handlers/commands/setCooldown.js
@@ -0,0 +1,42 @@
+const commandBase = require("./commandBase");
+
+class setCooldown extends commandBase
+{
+    constructor(models) {
+        super(models);
+        this.descriptionText = "Lets the user set his own cooldown"
+        this.helpText = "Usage: `!setCooldown [cooldown in ms] [userID (optional)]` | Lets a user set his own cooldown by default or set another's for him"
+        this.name = "setCooldown"
+    }
+
+    async execute(user, msg, args) {
+        const splitArgs = args.split(' ');
+        const newCooldown = splitArgs.shift()
+        const userID = splitArgs.join(' ');
+        if (newCooldown == ""){
+            await msg.channel.send("You need to include a cooldown in your command");
+            return;
+        }
+        if(userID === '') {
+            user.cooldown = newCooldown;
+            await user.save();
+            await msg.channel.send("Your cooldown has been updated!");
+            console.log('Cooldown for ' + msg.author.username + ' set to ' + user.cooldown);
+        } else {
+            var IDRegex = /<@!(.+)>/g;
+            var IDArray = IDRegex.exec(userID);
+            if(IDArray[1] != undefined) {
+                const targetUser = await models.Stamps.findOne({where: { username: IDArray[1] }});
+                if(targetUser != null) {
+                    targetUser.cooldown = newCooldown
+                    await targetUser.save();
+                    console.log('Cooldown for ' + targetUser.username + ' set to ' + targetUser.cooldown);
+                } else {
+                    console.log('User not found');
+                }
+            }
+        }
+    }
+}
+
+module.exports = setCooldown;
